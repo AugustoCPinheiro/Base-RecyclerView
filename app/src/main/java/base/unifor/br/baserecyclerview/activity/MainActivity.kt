9773cd544e3f5f609325c6454967d321b92aa6bd
AugustoCPinheiro <augustocepinheiro@gmail.com>
2018-12-22 19:31:43 -0200

    migration to androidx

--- app/src/main/java/base/unifor/br/baserecyclerview/activity/MainActivity.kt
+++ app/src/main/java/base/unifor/br/baserecyclerview/activity/MainActivity.kt
@@ -1,13 +1,12 @@
-package base.unifor.br.baserecyclerview
+package base.unifor.br.baserecyclerview.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import base.unifor.br.baserecyclerview.R
 import base.unifor.br.baserecyclerview.adapter.ConcreteAdapter
 import base.unifor.br.baserecyclerview.model.MyObject
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     val adapter = ConcreteAdapter()
